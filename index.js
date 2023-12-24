46483849594ae5c97f7b165866de92a5a603890a
husanjonmusayev <husanjonm279@gmail.com>
2023-12-24 12:01:11 +0500

    uy ishi

---
+++ index.js
@@ -0,0 +1,40 @@
+// uy ishi
+// 1 masala
+// let arg = [1,2,3,4,5,6,7];
+// let S = 0;
+// function sum (arg){
+// for (const item of arg) {
+//     if (item % 2 == 0){
+//         S += item**2
+//     }
+// }
+// return S
+// }
+// console.log(sum(arg));
+
+// ikkinchi masala
+// let mass = ['Aziz','Bahtior','Yaxyo']
+// let twoMas = [ ] 
+// mass.forEach((item) => {
+// twoMas.push(item.charAt())
+// })
+// console.log(twoMas);
+
+// uchinchi masala
+// let object = {num1:1, num2:2, num3:3,}
+// let object1 = 0;
+// for (const key in object) {
+//   object1 += object[key]
+// }
+// console.log(object1);
+
+// beshinchi masala
+// let n = 10;
+// let sum = 1;
+// for (let i = 1; i <= n; i++){
+//   if (i % 2 == 1){
+//     sum *= i
+//   }
+  
+// }
+// console.log(sum);
